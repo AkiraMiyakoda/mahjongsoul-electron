93b60a45afae477d67c3c298e91fa92347eda1e1
Akira Miyakoda <akira.miyakoda@gmail.com>
2022-01-26 20:54:41 +0900

    Save and restore window placement

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const electron = require("electron");
 const path = require("path");
+const Store = require("electron-store")
+const store = new Store()
+
+const DEFAULT_PLACEMENT  = [ 50, 50, 1600, 900 ];
 
 electron.app.on("ready", () => {
     "use strict";
 
     const lock = electron.app.requestSingleInstanceLock();
     if (lock) {
+        const placement = store.get("window.placement")  || DEFAULT_PLACEMENT;
         const main_window = new electron.BrowserWindow({
-            x: 50,
-            y: 50,
-            width: 1600,
-            height: 900,
-            resizable: false,
+            x: placement[0],
+            y: placement[1],
+            width:  placement[2],
+            height: placement[3],
             title: "雀魂 -じゃんたま-",
             icon: path.join(__dirname, "./icon.png"),
         });
         main_window.setMenu(null);
         main_window.loadURL("https://game.mahjongsoul.com/index.html");
 
+        main_window.on("close", () => {
+            store.set("window.placement", [ ...main_window.getPosition(), ...main_window.getSize() ]);
+        })
+        
         electron.app.on("second-instance", () => {
             if (main_window.isMinimized()) {
                 main_window.restore();
