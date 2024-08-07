77db07cec9802f812a53f39c3082ddb7fe67b881
Akira Miyakoda <akira.miyakoda@gmail.com>
2024-08-08 08:09:32 +0900

    Change indent size from 2 to 4

--- index.js
+++ index.js
@@ -11,33 +11,33 @@ const store = new Store();
 const DEFAULT_PLACEMENT = [50, 50, 1600, 900];
 
 app.on("ready", () => {
-  "use strict";
+    "use strict";
 
-  const lock = app.requestSingleInstanceLock();
-  if (lock) {
-    const placement = store.get("window.placement") || DEFAULT_PLACEMENT;
-    const main_window = new BrowserWindow({
-      x: placement[0],
-      y: placement[1],
-      width: placement[2],
-      height: placement[3],
-      title: "雀魂 -じゃんたま-",
-      icon: path.join(__dirname, "./icon.png"),
-    });
-    main_window.setMenu(null);
-    main_window.loadURL("https://game.mahjongsoul.com/index.html");
+    const lock = app.requestSingleInstanceLock();
+    if (lock) {
+        const placement = store.get("window.placement") || DEFAULT_PLACEMENT;
+        const main_window = new BrowserWindow({
+            x: placement[0],
+            y: placement[1],
+            width: placement[2],
+            height: placement[3],
+            title: "雀魂 -じゃんたま-",
+            icon: path.join(__dirname, "./icon.png"),
+        });
+        main_window.setMenu(null);
+        main_window.loadURL("https://game.mahjongsoul.com/index.html");
 
-    main_window.on("close", () => {
-      store.set("window.placement", [...main_window.getPosition(), ...main_window.getSize()]);
-    });
+        main_window.on("close", () => {
+            store.set("window.placement", [...main_window.getPosition(), ...main_window.getSize()]);
+        });
 
-    app.on("second-instance", () => {
-      if (main_window.isMinimized()) {
-        main_window.restore();
-      }
-      main_window.focus();
-    });
-  } else {
-    app.quit();
-  }
+        app.on("second-instance", () => {
+            if (main_window.isMinimized()) {
+                main_window.restore();
+            }
+            main_window.focus();
+        });
+    } else {
+        app.quit();
+    }
 });
