b21e5e847fafd1f5675d6ee5c715509bd72c245a
Akira Miyakoda <akira.miyakoda@gmail.com>
2024-09-24 22:30:16 +0900

    Fix broken build

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-// Copyright (c) 2022 Akira Miyakoda
+// Copyright (c) 2024 Akira Miyakoda
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-const { app, BrowserWindow } = require("electron");
-const path = require("path");
-const Store = require("electron-store");
-const store = new Store();
+"use strict";
+
+import { app, BrowserWindow } from "electron";
+import Store from "electron-store";
+import { URL } from "url";
 
 const DEFAULT_PLACEMENT = [50, 50, 1600, 900];
+const GAME_URL = new URL("https://game.mahjongsoul.com/index.html");
 
-app.on("ready", () => {
-    "use strict";
+app.on("ready", async () => {
+    const store = new Store();
 
     const lock = app.requestSingleInstanceLock();
     if (lock) {
         const placement = store.get("window.placement") || DEFAULT_PLACEMENT;
-        const main_window = new BrowserWindow({
+        const mainWindow = new BrowserWindow({
             x: placement[0],
             y: placement[1],
             width: placement[2],
             height: placement[3],
             title: "雀魂 -じゃんたま-",
-            icon: path.join(__dirname, "./icon.png"),
         });
-        main_window.setMenu(null);
-        main_window.loadURL("https://game.mahjongsoul.com/index.html");
+        mainWindow.setMenu(null);
 
-        main_window.on("close", () => {
-            store.set("window.placement", [...main_window.getPosition(), ...main_window.getSize()]);
+        mainWindow.on("close", () => {
+            store.set("window.placement", [...mainWindow.getPosition(), ...mainWindow.getSize()]);
         });
 
         app.on("second-instance", () => {
-            if (main_window.isMinimized()) {
-                main_window.restore();
+            if (mainWindow.isMinimized()) {
+                mainWindow.restore();
             }
-            main_window.focus();
+            mainWindow.focus();
         });
+
+        await mainWindow.loadURL(GAME_URL.href);
     } else {
         app.quit();
     }
